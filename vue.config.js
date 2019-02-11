2aba9eb707ef6bedb4a6afbaa0be73924453024d
andy.rothwell <rothwell.andy@gmail.com>
2019-02-11 16:59:32 -0500

    adds fortawesome vue-fontawesome

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ module.exports = {
   publicPath: '/ct3/',
   chainWebpack: (config) => {
     config.plugins.delete('prefetch')
-  }
+  },
+  transpileDependencies: [
+    // can be string or regex
+    '@cityofphiladelphia/mapboard',
+    '@cityofphiladelphia/phila-vue-comps',
+    '@cityofphiladelphia/phila-vue-mapping',
+  ]
 }
